782fd91f8bd5075673d76fb7c6419b4e680c0607
minzunim <greenteapig6@gmail.com>
2025-03-10 00:55:13 +0900

    @apollo/server 4 migration

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -6,10 +6,12 @@ import * as resolvers from './resolvers';
 // schema 통합
 export const schema = makeSchema({
     types: [types, resolvers],
+    // nexus schema, type 파일 생성 경로
     outputs: {
         schema: join(__dirname, 'generated/schema.graphql'),
         typegen: join(__dirname, 'generated/nexus.ts'),
     },
+    // graphql resolver 에서 사용할 context 설정
     contextType: {
         export: 'Context',
         module: require.resolve('../context'),
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,37 @@
-import { ApolloServer } from 'apollo-server';
-import { schema } from './graphql/schema';
-import { createContext } from './context';
 
-const server = new ApolloServer({
-    schema,
-    context: createContext,
-});
+const { ApolloServer } = require("@apollo/server"); // @apollo/server 4버전 migration
+const { startStandaloneServer } = require("@apollo/server/standalone");
+import { schema } from "./graphql/schema"; // Nexus 스키마 사용
+import { PrismaClient } from "@prisma/client";
 
-server.listen({ port: 4000 }).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-});
+// const TrackAPI = require("./datasources/track-api");
+const port = process.env.PORT || 4000;
+
+const prisma = new PrismaClient();
+
+async function startApolloServer() {
+    const server = new ApolloServer({ schema });
+
+    // prisma 사용
+    const { url } = await startStandaloneServer(server, {
+        context: async () => ({ prisma }),
+    });
+
+    console.log(`
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer();
+
+// apollo-server 3 코드
+// const server = new ApolloServer({
+//     schema,
+//     context: createContext,
+// });
+
+// server.listen({ port: 4000 }).then(({ url }) => {
+//     console.log(`🚀 Server ready at ${url}`);
+// });
