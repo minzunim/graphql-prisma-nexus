0109e530f4c6b0a42412138b9698868995c4e413
Minju Hwang <greenteapig6@gmail.com>
2025-03-10 16:51:10 +0900

    member mutation 추가, graphql-ws, ws 설치

--- src/graphql/resolvers/member.ts
+++ src/graphql/resolvers/member.ts
@@ -1,4 +1,4 @@
-import { extendType } from 'nexus';
+import { extendType, intArg, nonNull } from 'nexus';
 import { Context } from '../../context';
 
 export const MemberQuery = extendType({
@@ -9,24 +9,47 @@ export const MemberQuery = extendType({
             resolve: async (_parent, _args, ctx: Context) => {
                 return ctx.prisma.member.findMany();
             },
+        }),
+
+        t.field('member', {
+            type: 'Member',
+            args: {
+                idx: 'Int'
+            },
+            resolve: async (_parent, args, ctx: Context) => {
+
+                if (args.idx == null) {
+                    throw new Error("idx 값이 없습니다.");
+                }
+            
+                return ctx.prisma.member.findUnique({
+                where: { idx: args.idx }
+            })
+        }
+    })
+    }
+});
+
+export const MemberMutation = extendType({
+    type: 'Mutation',
+    definition(t) {
+        t.field('createMember', {
+            type: 'Member',
+            args: {
+                id: 'String',
+                name: 'String',
+                email: 'String',
+            },
+            resolve: async (_parent, { id, name, email }, ctx: Context) => {
+
+                if (!id || !name) {
+                    throw new Error("id, 이름이 없습니다.");
+                }
+
+                return ctx.prisma.member.create({
+                    data: { id, name, email },
+                });
+            },
         });
     },
 });
-
-// export const MemberMutation = extendType({
-//     type: 'Mutation',
-//     definition(t) {
-//         t.field('createMember', {
-//             type: 'Member',
-//             args: {
-//                 name: 'String',
-//                 email: 'String',
-//             },
-//             resolve: async (_parent, { name, email }, ctx: Context) => {
-//                 return ctx.prisma.member.create({
-//                     data: { name, email },
-//                 });
-//             },
-//         });
-//     },
-// });
